457b6afd42bc1a17c889bfb077a69e0f15a3574d
AdrienPinheiro <adrienpnrpro@gmail.com>
2022-03-26 14:40:09 +0100

    2eme commit page accueil remplis a faire: - font-family - css - responsive - html anglais

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const MINIMUM_SEARCH_LENGTH = 3;
 
-let listBox = document.querySelector('.resultSearch');
+let infoBox = document.querySelector('.resultSearch');
 let input = document.querySelector("#search");
 let boxes = []
 
@@ -11,10 +11,16 @@ Array.prototype.remove = Array.prototype.remove || function(){this.splice(0, thi
  * @param  {string} e
  */
 function searchBox(e){
-    let search = input.value.trim() || e
+    let search;
+    if(!e){
+        search = input.value.trim()
+    } else {
+        search = e
+        input.value = ""
+    }
     if(search.length >= MINIMUM_SEARCH_LENGTH){
         let maj = search.slice(0,1).toUpperCase()
-        let other = search.slice(1)
+        let other = search.slice(1).toLowerCase()
         let searchValue = maj + other
         loadBox(searchValue);
     }
@@ -35,7 +41,11 @@ async function loadBox(searchValue){
         if(data[i].city == searchValue ){
             boxes.push(data[i])
         } else{
-            listBox.innerHTML = `<p class="notFound">Pas de box trouvé</p>`
+            infoBox.innerHTML = `
+            <p class="notFound">Nous n'avons malheureusement pas de box disponible.
+            Vous pouvez néanmoins nous contacter pour en réserver !</p>
+            <p class="contactPhone"><i class="fas fa-phone-alt"></i> 05 62 19 19 79.</p>
+            <p class="otherBox">Nous avons aussi des box situés dans plusieurs autres villes ! <i class="fas fa-arrow-down"></i></p>`
         }
     }
     if (boxes[0].city == searchValue) return showListBox(boxes)
@@ -46,23 +56,38 @@ async function loadBox(searchValue){
  * @param  {Array} boxes
  */
 function showListBox(boxes){
-    listBox.innerHTML = "";
+    infoBox.innerHTML = `
+    <div class="topResultSearch">
+        <h3>Garde Meuble ${boxes[0].city}</h3>
+        <p>La solution à vos besoins d’espace et de stockage de vos affaires, c’est le garde-meubles ou le self-stockage à ${boxes[0].city}.</p>
 
+        <p>Dans un garde meubles, vous même ou votre déménageur transporte vos biens et cartons vers un bâtiment sous surveillance, où vos biens seront stockés dans un boc pendant une période
+        déterminée. Dans un garde meuble classique dès que vos affaires sont dans le box, vous ne pouvez plus y accéder jusqu’à la fin de la durée du stockage.</p>
+        
+        <p>La réponse la plus efficace est la nouvelle génération de garde-meubles: le self stockage. Un box individuel accueille vos biens et vous en avez la clef.
+        L’avantage du self-stockage, c’est que quand vous le souhaitez, vous pouvez vous rendre à votre box pour ramener ou déposer des meubles et cartons.
+        l’accès est illimité et gratuit, tous les jours.</p>
+        
+        <p>Voici notre sélection de sites ${boxes[0].resident} où vous pourrez ranger vos affaires: meubles, matériel ou affaires de sport, cartons... en toute sécurité et flexibilité, puisque vous louez sans aucun
+        engagement de durée, et pourrez partir quand vous le voudrez.</p>
+    </div>
+    `
+    let listBox = document.createElement("div");
+    listBox.classList.add('listBox');
+    infoBox.appendChild(listBox)
     boxes.map((box) => {
         let cardBox = document.createElement("div");
         cardBox.classList.add('cardBox')
         cardBox.dataset.id = box.id;
         cardBox.innerHTML = `
-        <div class="cardBoxTop">
-            <h4>${box.localisation}</h4>
-            <p>${box.address}</p>
-            <p>${box.phone}</p>
-            <p>Horaires des bureaux :</p>
-            <p>${box.hourly}</p>
-        <div>
+        <h4>${box.localisation}</h4>
+        <p class="address">${box.address}</p>
+        <p class="phone"><i class="fas fa-phone-alt"></i> ${box.phone}</p>
+        <h4>Horaires des bureaux :</h4>
+        <p class="hourly">${box.hourlyWeek}</p>
+        <p class="hourly">${box.hourlyWeekEnd}</p>
         `;
         listBox.appendChild(cardBox);
     });
     boxes.remove();
-    loadOneBox();
-}
+} 
